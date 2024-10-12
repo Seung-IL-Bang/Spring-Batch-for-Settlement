49093b2e99b74935d93c8c2c11dd668da2279e54
Seungil <seungilbang@khu.ac.kr>
2024-10-13 01:08:46 +0900

    @EnableBatchProcessing 추

---
+++ src/main/java/com/project/settlement_batch/config/BatchConfig.java
@@ -0,0 +1,7 @@
+package com.project.settlement_batch.config;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+
+@EnableBatchProcessing
+public class BatchConfig {
+}

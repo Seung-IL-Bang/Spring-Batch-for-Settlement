3d7735ade6accff39479d6c1f65e0af0a2e3f9f4
Seungil <seungilbang@khu.ac.kr>
2024-10-12 19:48:44 +0900

    initial commit

---
+++ src/main/java/com/project/settlement_batch/SettlementBatchApplication.java
@@ -0,0 +1,13 @@
+package com.project.settlement_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettlementBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SettlementBatchApplication.class, args);
+	}
+
+}
